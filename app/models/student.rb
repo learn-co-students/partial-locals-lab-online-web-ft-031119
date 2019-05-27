64c7c6afa57f07fa2b2fbce81eedb1bfa6548db1
Jon <jhharwood88@gmail.com>
2019-05-27 19:03:41 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,22 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :birthday, presence: true
+
+  # def self.search(name)
+  # 	if name == "" 
+  # 		self.all
+  # 	else
+  # 		self.all.select {|student|student.name.downcase.include?(name)}
+  # 	end
+  # end
+  def self.search(query)
+    if query.present?
+      where('NAME like ?', "%#{query}%")
+    else
+      self.all
+    end
+  end
 end
